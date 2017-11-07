18de402142541b4a1ada89e01d1e0b56e3bf9814
QueensKid <andres.ibarra0716@gmail.com>
2017-11-07 13:45:00 -0800

    book_app day 1 complete, we think

--- server.js
+++ server.js
@@ -1,23 +1,71 @@
 'use strict';
-const express = require('express');
-const cors = require('cors');
+
 const pg = require('pg');
+const express = require('express');
+const fs = require('fs');
+// const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
 const app = express();
 const PORT = process.env.PORT;
 const CLIENT_URL = process.env.CLIENT_URL;
-//const conString = `postgres://USER:4166@${PORT}`;
-const client = new pg.Client(process.env.DATABASE_URL);
+
+const conString = 'postgres://postgres:081583@localhost:5432/postgres';
+const client = new pg.Client(conString);
+client.connect();
 app.use(cors());
 
 
-app.get('/test', (request, response) => response.send('Hello World'));
+app.get('/test', (request, response) => response.send('Hello World!'));
 
-app.get('/tasks', (req, res) => {
-  client.query(`SELECT * from tasks;`)
-    .then(results => res.send(results.rows))
-    .catch(console.error);
+app.get('/api/v1/books', (request, response) => {
+  client.query(`
+     SELECT book_id, title, author, image_url FROM books;
+     `)
+    .then(result => response.send(result.rows))
+    .catch(err => console.log(err));
 });
 
-app.get('*', (req, res) => res.redirect(CLIENT_URL));
+app.get('*', (request, response) => response.redirect(CLIENT_URL));
 
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+loadDB();
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+
+function loadBooks() {
+  console.log('************');
+  client.query(`SELECT count(*) FROM books`)
+    .then(result => {
+      if (!parseInt(result.rows[0].count)){
+        fs.readFile('data/books.json', function(err, file) {
+          JSON.parse(file.toString()).forEach(book => {
+            client.query(`
+              INSERT INTO
+              books(title, author, isbn, image_url, description)
+                VALUES($1, $2, $3, $4, $5);`,
+              [book.author, book.title, book.isbn, book.image_url, book.description]
+            )
+          })
+        })
+      }
+    })
+    .then(err => console.log('shits broke son'(err)));
+}
+
+function loadDB() {
+  client.query(`
+    CREATE TABLE IF NOT EXISTS
+    books (
+      book_id SERIAL PRIMARY KEY,
+      author VARCHAR(50),
+      title VARCHAR(50),
+      isbn VARCHAR(50),
+      image_url TEXT,
+      description TEXT
+    );`
+  )
+    .then(data => loadBooks(data))
+    .catch(err => console.log(err));
+}
