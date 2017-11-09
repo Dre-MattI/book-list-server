c8c605e7aa33606e17e3502612ef2d95b6c73a6a
QueensKid <andres.ibarra0716@gmail.com>
2017-11-09 10:14:11 -0800

    server.js additions, broke everything but we are working on it

--- server.js
+++ server.js
@@ -3,35 +3,62 @@
 const pg = require('pg');
 const express = require('express');
 const fs = require('fs');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 
 
 const app = express();
 const PORT = process.env.PORT;
 const CLIENT_URL = process.env.CLIENT_URL;
-const DATABASE_URL = process.env.DATABASE_URL;
-// 'postgres://postgres:081583@localhost:5432/booklist';
-const client = new pg.Client(DATABASE_URL);
 
+// const conString = 'postgres://postgres:1234@localhost:5432/postgres';
+const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
-client.on('error', err => console.error(err));
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 
 
-app.get('/test', (request, response) => response.send('F**k this Project'));
+app.get('/test', (request, response) => response.send('Hello World!'));
+
+
 
 app.get('/api/v1/books', (request, response) => {
   client.query(`
-     SELECT book_id, title, author, image_url FROM books;`)
+     SELECT book_id, title, author, image_url FROM books;
+     `)
     .then(result => response.send(result.rows))
     .catch(err => console.log(err));
 });
 
+app.get('/book/:id', (request, response) => {
+  client.query(`
+    SELECT * FROM books
+    WHERE book_id=$1;`,
+    [request.params.id]
+  )
+    .then(result => {
+      console.log(result);
+      response.send(result.rows)})
+    .catch(err => console.log(err));
+});
+
+app.post('/newbook', (request, response) => {
+  client.query(`
+    INSERT INTO books(title, author, isbn, image_url, description)
+    VALUES ($1, $2, $3, $4, $5);`,
+    [request.body.title, request.body.author, request.body.isbn, request.body.image_url, request.body.description]),
+  function(err) {
+    if (err) console.error(err);
+    response.send('insert complete');
+  }
+})
+
 app.get('*', (request, response) => response.redirect(CLIENT_URL));
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 loadDB();
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 function loadBooks() {
   client.query(`SELECT count(*) FROM books`)
@@ -41,15 +68,14 @@ function loadBooks() {
           JSON.parse(file.toString()).forEach(book => {
             client.query(`
               INSERT INTO
-              books(title, author, isbn, image_url)
-                VALUES($1, $2, $3, $4);`,
-              [book.author, book.title, book.isbn, book.image_url]
+              books(title, author, isbn, image_url, description)
+                VALUES($1, $2, $3, $4, $5);`,
+              [book.title, book.author, book.isbn, book.image_url, book.description]
             )
           })
         })
       }
     })
-    .then(err => console.log('shits broke son'(err)));
 }
 
 function loadDB() {
@@ -60,7 +86,8 @@ function loadDB() {
       author VARCHAR(50),
       title VARCHAR(50),
       isbn VARCHAR(50),
-      image_url TEXT
+      image_url TEXT,
+      description TEXT
     );`
   )
     .then(data => loadBooks(data))
