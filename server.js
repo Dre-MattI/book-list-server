e0f1013e34d8e7c82def08e3c2c98f6a37c91d25
QueensKid <andres.ibarra0716@gmail.com>
2017-11-10 13:27:46 -0800

    bodyparser

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ const express = require('express');
 const fs = require('fs');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
-
 const app = express();
 const PORT = process.env.PORT;
 const CLIENT_URL = process.env.CLIENT_URL;
@@ -15,7 +13,6 @@ client.connect();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
-
 
 app.get('/test', (request, response) => response.send('Hello World!'));
 
@@ -41,8 +38,8 @@ app.get('/book/:id', (request, response) => {
     .catch(err => console.log(err));
 });
 
-app.put('/book/update/:id', (request, response) => {
-  console.log('this is working');
+app.put('/book/update/:id', bodyParser, (request, response) => {
+  console.log(request.body);
   let {title, author, isbn, image_url, description} = request.body;
   client.query(`
     UPDATE books
@@ -55,7 +52,7 @@ app.put('/book/update/:id', (request, response) => {
     .catch(err => console.error(err))
 })
 
-app.post('/book/new', (request, response) => {
+app.post('/book/new', bodyParser, (request, response) => {
   client.query(`
     INSERT INTO books(title, author, isbn, image_url, description)
     VALUES ($1, $2, $3, $4, $5);`,
@@ -83,36 +80,36 @@ loadDB();
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 
-function loadBooks() {
-  client.query(`SELECT count(*) FROM books`)
-    .then(result => {
-      if (!parseInt(result.rows[0].count)){
-        fs.readFile('data/books.json', function(err, file) {
-          JSON.parse(file.toString()).forEach(book => {
-            client.query(`
-              INSERT INTO
-              books(title, author, isbn, image_url, description)
-                VALUES($1, $2, $3, $4, $5);`,
-              [book.title, book.author, book.isbn, book.image_url, book.description]
-            )
-          })
-        })
-      }
-    })
-}
-
-function loadDB() {
-  client.query(`
-    CREATE TABLE IF NOT EXISTS
-    books (
-      book_id SERIAL PRIMARY KEY,
-      author VARCHAR(50),
-      title VARCHAR(50),
-      isbn VARCHAR(50),
-      image_url TEXT,
-      description TEXT
-    );`
-  )
-    .then(data => loadBooks(data))
-    .catch(err => console.log(err));
-}
+// function loadBooks() {
+//   client.query(`SELECT count(*) FROM books`)
+//     .then(result => {
+//       if (!parseInt(result.rows[0].count)){
+//         fs.readFile('data/books.json', function(err, file) {
+//           JSON.parse(file.toString()).forEach(book => {
+//             client.query(`
+//               INSERT INTO
+//               books(title, author, isbn, image_url, description)
+//                 VALUES($1, $2, $3, $4, $5);`,
+//               [book.title, book.author, book.isbn, book.image_url, book.description]
+//             )
+//           })
+//         })
+//       }
+//     })
+// }
+//
+// function loadDB() {
+//   client.query(`
+//     CREATE TABLE IF NOT EXISTS
+//     books (
+//       book_id SERIAL PRIMARY KEY,
+//       author VARCHAR(50),
+//       title VARCHAR(50),
+//       isbn VARCHAR(50),
+//       image_url TEXT,
+//       description TEXT
+//     );`
+//   )
+//     .then(data => loadBooks(data))
+//     .catch(err => console.log(err));
+// }
